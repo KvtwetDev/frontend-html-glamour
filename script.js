d04f9160ac8c2fa49340ec105bbf61c368173a60
kvtwet <alves2.eduardo@gmail.com>
2024-12-18 19:56:51 -0300

    -- Criação da responsividade para mobile

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     });
   });
   
+  document.getElementById("contact").addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    // Coletar os dados do formulário
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    // Número do WhatsApp (adicione o número desejado com o código do país)
+    const phoneNumber = "5511999999999";
+  
+    // Criar a mensagem no formato adequado
+    const whatsappMessage = `Olá, meu nome é ${name}. Meu email é ${email}. Mensagem: ${message}`;
+  
+    // Criar a URL do WhatsApp
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      whatsappMessage
+    )}`;
+  
+    // Redirecionar para o WhatsApp
+    window.open(whatsappURL, "_blank");
+  });
+
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+
+  hamburger.addEventListener("click", () =>
+    nav.classList.toggle("active")
+);
+  
